24add6b2111df66ce02948456593d0dd5072bb64
Wesley <wesleybruno910@gmail.com>
2023-05-24 13:15:42 -0300

    lógica descriptar concluida

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const textInput = document.getElementById("text-input");
 const textOutput = document.getElementById("text-output");
 const criptografaBtn = document.getElementById("criptografar-btn");
+const descriptografaBtn = document.getElementById("descriptografar-btn");
 
 
 criptografaBtn.addEventListener('click', function() {
@@ -11,6 +12,14 @@ criptografaBtn.addEventListener('click', function() {
     `
 });
 
+descriptografaBtn.addEventListener('click', function() {
+    removeImagensParaExibirOutput();
+    textOutput.innerHTML = `
+    <p>${descripta(textInput.value)}</p>
+    <input class="btn" id="copiar-btn" type="button" value="copiar">`
+});
+
+
 function removeImagensParaExibirOutput() {
     let imagens = textOutput.getElementsByTagName("img");
     let qtdImages = Array.from(imagens);
@@ -19,6 +28,8 @@ function removeImagensParaExibirOutput() {
         img.remove();
     });
 }
+
+console.log(descripta("haingzhoberufat"));
 
 function criptografa(textInput) {
     let strCriptada="";
@@ -44,6 +55,57 @@ function criptografa(textInput) {
         } 
     }
     return strCriptada;
+}
+
+function descripta(textInput) {
+    let strDescriptada = "";
+    for(let i = 0; i < textInput.length; i++) {
+        switch(textInput[i]) {
+            case 'e':
+                if(textInput.slice(i, i+5) === "enter") {
+                    strDescriptada+="e";
+                    i+=4
+                } else {
+                    strDescriptada+="";
+                }
+            break;
+            case 'i':
+                if(textInput.slice(i, i+4) === "imes") {
+                    strDescriptada+="i";
+                    i+=3;
+                } else {
+                    strDescriptada+="";
+                }
+            break;
+            case 'a':
+                if(textInput.slice(i, i+2) === "ai") {
+                    strDescriptada+="a";
+                    i+=1;
+                } else {
+                    strDescriptada+="";
+                }
+            break;
+            case 'o':
+                if(textInput.slice(i, i+4) === "ober") {
+                    strDescriptada+="o";
+                    i+=3;
+                } else {
+                    strDescriptada+="";
+                }
+            break;
+            case 'u':
+                if(textInput.slice(i, i+4) === "ufat") {
+                    strDescriptada+="u";
+                    i+=3;
+                } else {
+                    strDescriptada+="";
+                }
+            break;
+            default:
+                strDescriptada+=textInput[i];
+        }
+    }
+    return strDescriptada;
 }
 
 
